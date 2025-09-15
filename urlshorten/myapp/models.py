497b92a6f4ab3e0eac60807e99045cbfda7d8a24
Bharath Sivanesh.S <132342144+Bharathsivanesh@users.noreply.github.com>
2025-09-15 22:31:15 +0530

    feat/creted model for url shortner

--- urlshorten/myapp/models.py
+++ urlshorten/myapp/models.py
@@ -1,3 +1,8 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+class Urlshorten(models.Model):
+    Long_url=models.TextField()
+    Short_url=models.TextField()
+    Created_at = models.DateTimeField(default=timezone.now)
