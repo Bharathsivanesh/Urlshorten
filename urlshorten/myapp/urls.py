ccf659fe4f8d7d69b49958c72f2481ef111c0ec0
Bharath Sivanesh.S <132342144+Bharathsivanesh@users.noreply.github.com>
2025-09-17 23:50:54 +0530

    feat/implement deleted view

--- urlshorten/myapp/urls.py
+++ urlshorten/myapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('post/',views.store_url),
-path('get/',views.get_url)
+path('get/',views.get_url),
+    path('delete/<int:id>/',views.delete_url)
 ]
